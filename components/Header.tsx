54e4c3cf6b8cf4dc48a92824bfa8858beca5100d
basthep <abdulbasitherayampattil@gmail.com>
2024-10-24 12:59:44 +0530

    third commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,5 +10,99 @@ import { Badge } from "./ui/badge"
 import {Web3Auth} from '@web3auth/modal'
 import {CHAIN_NAMESPACES,IProvider,WEB3AUTH_NETWORK} from '@web3auth/base'
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
+import { createUser, getUnreadNotification, getUserByEmail } from "@/utils/db/actions"
 
 const clientId = process.env.WEB3_AUTH_CLIENT_ID
+
+const chainConfig = {
+    chainNamespace:CHAIN_NAMESPACES.EIP155,
+    chainId:0xaa36a7,
+    rpcTarget:'https://rpc.ankr.com/eth_sepolia',
+    displayName:'Sepolia Testnet',
+    blockExplorerUrl:'https://sepolia.etherscan.io',
+    ticker:'ETH',
+    tickerName:'Ethereum',
+    logo:'https://assets.web3auth.io/evm-chains/sepolia.png'
+}
+
+const privateKeyProvider=new EthereumPrivateKeyProvider({
+    config:chainConfig
+})
+
+const web3Auth=new Web3Auth({
+    clientId,
+    web3AuthNetwork:WEB3AUTH_NETWORK.TESTNET,
+    privateKeyProvider
+})
+
+interface HeaderProps{
+    onMenuClick:()=>void;
+    totalEarnings:number;
+}
+
+export default function Header({onMenuClick,totalEarnings}:HeaderProps){
+    const [provider,setProvider]=useState<IProvider | null>(null)
+    const [loggedeIn,setLoggedeIn]=useState(false)
+    const [loading,setLoading]=useState(true)
+    const [userInfo,setUserInfo]=useState<any>(null)
+    const pathname=usePathname()
+    const [notification,setNotification]=useState<Notification[]>([])
+    const [balance,setBalance]=useState(0)
+
+    useEffect(()=>{
+        const init=async()=>{
+            try {
+              await web3Auth.initModal();
+              setProvider(web3Auth.provider) 
+              
+              if(web3Auth.connected){
+                setLoggedeIn(true)
+                const user=await web3Auth.getUserInfo();
+                setUserInfo(user)
+                if(user.email){
+                    localStorage.setItem('userEmail',user.email)
+                    try {
+                        await createUser(user.email,user.name || "Anonymous user")                        
+                    } catch (error) {
+                       console.error('Error creating user',error);
+                        
+                    }
+
+                }
+              }
+            } catch (error) {
+                console.error('Error initializing web3auth',error);
+                
+            }finally{
+                setLoading(false)
+            }
+        }
+        init()
+    },[])
+
+    useEffect(()=>{
+        const fetchNotifications = async ()=>{
+            if(userInfo && userInfo.email){
+                const user= await getUserByEmail(userInfo.email)
+                if (user) {
+                    const unreadNotifications=await getUnreadNotification(user.id)
+                    setNotification(unreadNotifications)
+                }
+            }
+        }
+        fetchNotifications();
+        const notificationInterval = setInterval(fetchNotifications,30000)
+        return ()=>clearInterval(notificationInterval)
+    },[userInfo])
+
+    useEffect(()=>{
+        const fetchUserBalance = async () =>{
+            if (userInfo && userInfo.email) {
+                const user= await getUserByEmail(userInfo.email);
+                if (user) {
+                    const userBalance = 
+                }
+            }
+        }
+    })
+}
